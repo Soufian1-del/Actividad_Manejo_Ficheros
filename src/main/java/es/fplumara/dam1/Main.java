d3fe0783b99c3c43d6364cc80e246c3bafe5adb0
sourrxz <sourrxz@gmail.com>
2026-02-09 12:24:07 +0100

    empezado el nuevo bot de discord

--- src/main/java/es/fplumara/dam1/Main.java
+++ src/main/java/es/fplumara/dam1/Main.java
@@ -1,17 +1,25 @@
 package es.fplumara.dam1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //Leer todas las líneas de un fichero como string, si falla puede ser debido a un error en la ruta, requiere de un throws IOException
+        Path path = Path.of("C:\\Users\\SoufianAhidarDakkaki\\actividades_ficheros\\src\\main\\java\\es\\fplumara\\dam1\\datos.txt");
+        String contenido = Files.readString(path);
+        System.out.println(contenido);
+        //Leer todas las líneas de un fichero en una lista
+        Path pathLista = Path.of("C:\\Users\\SoufianAhidarDakkaki\\actividades_ficheros\\src\\main\\java\\es\\fplumara\\dam1\\datos.txt");
+        List<String> lineas = Files.readAllLines(pathLista);
+        Integer contador = 0;
+        for (String linea : lineas) {
+            System.out.println(contador + " " + linea);
+            contador++;
         }
-    }
+}
 }
