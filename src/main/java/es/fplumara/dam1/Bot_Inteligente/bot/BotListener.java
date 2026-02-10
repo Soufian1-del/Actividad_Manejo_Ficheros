d231c2b970bb74442dd4f1867f0b6540ecb92095
sourrxz <sourrxz@gmail.com>
2026-02-10 12:31:59 +0100

    añadido

--- src/main/java/es/fplumara/dam1/Bot_Inteligente/bot/BotListener.java
+++ src/main/java/es/fplumara/dam1/Bot_Inteligente/bot/BotListener.java
@@ -1,11 +1,29 @@
 package es.fplumara.dam1.Bot_Inteligente.bot;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class BotListener extends ListenerAdapter {
     @Override
-    public void SlashCommandInteractionEvent(SlashCommandInteractionEvent event){
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        // Ignorar mensajes de otros bots
+        if (event.getAuthor().isBot()) return;
+
+        String mensaje = event.getMessage().getContentRaw();
+        String usuario = event.getAuthor().getName();
+
+
+
+
+        System.out.println(event.getChannel().getName()+" - " +usuario + ": " + mensaje);
+
+
+
+
+
+
 
     }
 }
